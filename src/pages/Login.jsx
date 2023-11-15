2f5ebdeddadbf4796747d9d372c1d8ff6bfa456b
Andriawan Firmansyah <andriawnfirmansyah@gmail.com>
2023-11-15 12:07:48 +0700

    test

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,15 @@
 
 export default function Login() {
     return (
-        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 lg:py-40">
+        <div className="flex flex-col justify-center flex-1 min-h-full px-6 py-12 lg:px-8 lg:py-40">
             <div className="sm:mx-auto sm:w-full sm:max-w-sm">
                 <img
-                    className="mx-auto h-10 w-auto"
+                    className="w-auto h-10 mx-auto"
                     src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
                     alt="Your Company"
                 />
-                <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                    Sign in to your account
+                <h2 className="mt-10 text-2xl font-bold leading-9 tracking-tight text-center text-gray-900">
+                    Sign in to your account mate!
                 </h2>
             </div>
 
@@ -64,7 +64,7 @@ export default function Login() {
                     </div>
                 </form>
 
-                <p className="mt-10 text-center text-sm text-gray-900">
+                <p className="mt-10 text-sm text-center text-gray-900">
                     <p>----------------------- <span className="font-bold">Or continue with</span> -----------------------</p>
                 </p>
                 <div>
