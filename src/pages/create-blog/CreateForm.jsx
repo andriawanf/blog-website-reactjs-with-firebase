49b3c857a5894de54ca36d648363591e54d4d27a
Andriawan Firmansyah <andriawnfirmansyah@gmail.com>
2023-11-15 23:00:20 +0700

    feat: user can create blog and post to firebase

---
+++ src/pages/create-blog/CreateForm.jsx
@@ -0,0 +1,152 @@
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { addDoc, collection } from "firebase/firestore"
+import { auth, db } from "../../config/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Dialog, Transition } from '@headlessui/react';
+import { Fragment, useState } from 'react'
+
+export default function CreateForm() {
+    const [user] = useAuthState(auth);
+
+    const schema = yup.object().shape({
+        title: yup.string().required("please add the title first!"),
+        description: yup.string().required("You must add a description!"),
+    });
+
+    const { register, handleSubmit, resetField, formState: { errors } } = useForm({
+        resolver: yupResolver(schema),
+    });
+
+    const postRef = collection(db, "blogPosts");
+
+    const onCreatePost = async (data) => {
+        await addDoc(postRef, {
+            ...data,
+            username: user?.displayName,
+            user_id: user?.uid
+        });
+        resetField("title");
+        resetField("description");
+    }
+
+    let [isOpen, setIsOpen] = useState(false)
+
+    function closeModal() {
+        setIsOpen(false)
+    }
+
+    function openModal() {
+        setIsOpen(true)
+    }
+
+    return (
+        <>
+            <form className="container" onSubmit={handleSubmit(onCreatePost)}>
+                <div className="relative px-6 space-y-12 pt-14 lg:px-40 lg:pt-44">
+                    <div className="pb-12">
+                        <h2 className="text-3xl font-semibold leading-7 text-gray-900">Create Blog</h2>
+                        <p className="mt-1 text-sm leading-6 text-gray-600">
+                            This blog will be displayed publicly so make it something specials.
+                        </p>
+
+                        <div className="grid grid-cols-1 mt-10 gap-x-6 gap-y-8 sm:grid-cols-6">
+                            <div className="sm:col-span-4">
+                                <label htmlFor="title" className="block text-sm font-medium leading-6 text-gray-900">
+                                    Title
+                                </label>
+                                <div className="mt-2">
+                                    <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-400 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                                        <input
+                                            type="text"
+                                            name="title"
+                                            id="title"
+                                            {...register("title")}
+                                            className="block flex-1 border-0 bg-transparent py-1.5 px-3.5 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                            placeholder="ex. Breaking News!"
+                                        />
+                                    </div>
+                                    <p className="mt-2 text-sm text-red-600">{errors.title?.message}</p>
+                                </div>
+                            </div>
+
+                            <div className="col-span-full">
+                                <label htmlFor="description" className="block text-sm font-medium leading-6 text-gray-900">
+                                    Description
+                                </label>
+                                <div className="mt-2">
+                                    <textarea
+                                        id="description"
+                                        name="description"
+                                        rows={5}
+                                        {...register("description")}
+                                        className="block w-full rounded-md border-0 py-1.5 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-400 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                        defaultValue={''}
+                                    />
+                                </div>
+                                <p className="mt-2 text-sm text-red-600">{errors.description?.message}</p>
+                                <p className="mt-3 text-sm leading-6 text-gray-600">Write a few sentences about yourself.</p>
+                            </div>
+                            <button className="text-base text-white font-semibold px-3.5 py-2.5 rounded-lg bg-indigo-600 hover:bg-indigo-500" onClick={() => {}}>Post blog</button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+            <Transition appear show={isOpen} as={Fragment}>
+                <Dialog as="div" className="relative z-10" onClose={closeModal}>
+                    <Transition.Child
+                        as={Fragment}
+                        enter="ease-out duration-300"
+                        enterFrom="opacity-0"
+                        enterTo="opacity-100"
+                        leave="ease-in duration-200"
+                        leaveFrom="opacity-100"
+                        leaveTo="opacity-0"
+                    >
+                        <div className="fixed inset-0 bg-black/25" />
+                    </Transition.Child>
+
+                    <div className="fixed inset-0 overflow-y-auto">
+                        <div className="flex items-center justify-center min-h-full p-4 text-center">
+                            <Transition.Child
+                                as={Fragment}
+                                enter="ease-out duration-300"
+                                enterFrom="opacity-0 scale-95"
+                                enterTo="opacity-100 scale-100"
+                                leave="ease-in duration-200"
+                                leaveFrom="opacity-100 scale-100"
+                                leaveTo="opacity-0 scale-95"
+                            >
+                                <Dialog.Panel className="w-full max-w-md p-6 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+                                    <Dialog.Title
+                                        as="h3"
+                                        className="text-lg font-medium leading-6 text-gray-900"
+                                    >
+                                        Payment successful
+                                    </Dialog.Title>
+                                    <div className="mt-2">
+                                        <p className="text-sm text-gray-500">
+                                            Your payment has been successfully submitted. We’ve sent
+                                            you an email with all of the details of your order.
+                                        </p>
+                                    </div>
+
+                                    <div className="mt-4">
+                                        <button
+                                            type="button"
+                                            className="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                                            onClick={closeModal}
+                                        >
+                                            Got it, thanks!
+                                        </button>
+                                    </div>
+                                </Dialog.Panel>
+                            </Transition.Child>
+                        </div>
+                    </div>
+                </Dialog>
+            </Transition>
+        </>
+    )
+}
---
+++ src/pages/create-blog/CreatePost.jsx
@@ -0,0 +1,8 @@
+import CreateForm from "./CreateForm"
+export default function CreatePost() {
+    return (
+        <div>
+            <CreateForm />
+        </div>
+    )
+}
