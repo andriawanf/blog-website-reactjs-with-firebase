977239fdb51f71b18548afc292363d0c1d0115e3
Andriawan Firmansyah <andriawnfirmansyah@gmail.com>
2023-11-15 22:58:45 +0700

    feat: add a route page create post

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Navbar from './components/Navbar';
 import PageNotFound from './pages/PageNotFound';
+import CreatePost from "./pages/create-blog/CreatePost";
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/login' element={<Login />} />
+          <Route path='/create-post' element={<CreatePost />} />
           <Route path='*' element={<PageNotFound />} />
         </Routes>
       </Router>
