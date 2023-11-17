2e87224b44935263b5b874af0c823c09c25a3f25
Andriawan Firmansyah <andriawnfirmansyah@gmail.com>
2023-11-18 01:03:55 +0700

    feat: adding input for tag and image cover

---
+++ src/components/Card-Blog.jsx
@@ -0,0 +1,34 @@
+/* eslint-disable react/prop-types */
+
+export default function CardBlog({ blog }) {
+    return (
+        <article className="flex flex-col items-start justify-between max-w-xl p-6 bg-white shadow-lg rounded-3xl">
+            <img src={blog.photo_cover} alt="" className="w-full mb-2 bg-cover h-44 rounded-xl"/>
+            <div className="flex items-center text-xs gap-x-4">
+                <time dateTime="2020-03-16" className="text-gray-500">{blog.created_at}</time>
+                <a href="#" className="relative rounded-full bg-blue-100 px-3 py-1.5 font-medium text-blue-500 hover:bg-gray-100">{blog.tag_blog}</a>
+            </div>
+            <div className="relative group">
+                <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-blue-500">
+                    <a href="#">
+                        <span className="absolute inset-0"></span>
+                        {blog.title}
+                    </a>
+                </h3>
+                <p className="mt-5 text-sm leading-6 text-gray-600 line-clamp-3">{blog.description}</p>
+            </div>
+            <div className="relative flex items-center mt-8 gap-x-4">
+                <img src={blog.foto} alt="" className="w-10 h-10 rounded-full bg-gray-50" />
+                <div className="text-sm leading-6">
+                    <p className="font-semibold text-gray-900">
+                        <a href="#">
+                            <span className="absolute inset-0"></span>
+                            {blog.username}
+                        </a>
+                    </p>
+                    <p className="text-gray-600">Co-Founder / CTO</p>
+                </div>
+            </div>
+        </article>
+    )
+}
--- src/pages/create-blog/CreateForm.jsx
+++ src/pages/create-blog/CreateForm.jsx
@@ -2,10 +2,21 @@ import { useForm } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { addDoc, collection } from "firebase/firestore"
-import { auth, db } from "../../config/firebase";
+import { auth, db, storage } from "../../config/firebase";
+import { ref, getDownloadURL, uploadBytes } from "firebase/storage";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { Dialog, Transition } from '@headlessui/react';
-import { Fragment, useState } from 'react'
+import { Fragment, useState } from 'react';
+import { PhotoIcon } from '@heroicons/react/24/solid'
+
+
+const tagsBlog = [
+    'Education',
+    'Otomotif',
+    'Financial',
+    'Marketing',
+    'Sport',
+]
 
 export default function CreateForm() {
     const [user] = useAuthState(auth);
@@ -13,23 +24,57 @@ export default function CreateForm() {
     const schema = yup.object().shape({
         title: yup.string().required("please add the title first!"),
         description: yup.string().required("You must add a description!"),
+        tag_blog: yup.string().required("You must choose one of the tag!"),
+        photo_cover: yup.string().required("You must choose a photo!"),
     });
 
     const { register, handleSubmit, resetField, formState: { errors } } = useForm({
         resolver: yupResolver(schema),
     });
 
-    const postRef = collection(db, "blogPosts");
+    const postRef = collection(db, "blogs-post");
+
+    // Display image from input user
+    const [image, setImage] = useState("");
+
+    const handleFileChange = async(e) => {
+        const file = e.target.files[0];
+
+        // You can perform additional validation, e.g., checking file type or size
+
+        if (file) {
+            const storageRef = ref(storage, `blog_images/${file.name}`);
+    
+            try {
+              // Upload the file to Firebase Storage
+              await uploadBytes(storageRef, file);
+        
+              // Get the download URL of the uploaded file
+              const downloadURL = await getDownloadURL(storageRef);
+        
+              // Update the state with the download URL
+              setImage(downloadURL);
+            } catch (error) {
+              console.error("Error uploading image to Firebase Storage:", error);
+            }
+        }
+    };
 
     const onCreatePost = async (data) => {
+        // Upload the image first
+        // const imageUrl = image ? await uploadImageToFirebase(image) : null;
         await addDoc(postRef, {
             ...data,
             username: user?.displayName,
-            user_id: user?.uid
+            user_id: user?.uid,
+            foto_user: user?.photoURL,
+            created_at: new Date().toLocaleDateString(),
+            photo_cover: image,
         });
-        openModal();
         resetField("title");
         resetField("description");
+        resetField("tag_blog");
+        resetField("photo_cover");
     }
 
     let [isOpen, setIsOpen] = useState(false);
@@ -42,6 +87,24 @@ export default function CreateForm() {
         setIsOpen(true)
     }
 
+    
+
+    // 
+    // Function to upload the image to Firebase Storage
+    // const uploadImageToFirebase = async (file) => {
+    //     const storageRef = ref(storage, `blog-images/${user?.uid}/${file.name}`);
+    //     const uploadTask = uploadBytesResumable(storageRef, file);
+
+    //     try {
+    //         await uploadTask;
+    //         const downloadURL = await getDownloadURL(storageRef);
+    //         return downloadURL;
+    //     } catch (error) {
+    //         console.error("Error uploading image: ", error);
+    //         return null;
+    //     }
+    // };
+
     return (
         <div>
             <form className="container" onSubmit={handleSubmit(onCreatePost)}>
@@ -53,6 +116,7 @@ export default function CreateForm() {
                         </p>
 
                         <div className="grid grid-cols-1 mt-10 gap-x-6 gap-y-8 sm:grid-cols-6">
+                            {/* Input Title */}
                             <div className="sm:col-span-4">
                                 <label htmlFor="title" className="block text-sm font-medium leading-6 text-gray-900">
                                     Title
@@ -71,7 +135,7 @@ export default function CreateForm() {
                                     <p className="mt-2 text-sm text-red-600">{errors.title?.message}</p>
                                 </div>
                             </div>
-
+                            {/* Input Description */}
                             <div className="col-span-full">
                                 <label htmlFor="description" className="block text-sm font-medium leading-6 text-gray-900">
                                     Description
@@ -89,7 +153,60 @@ export default function CreateForm() {
                                 <p className="mt-2 text-sm text-red-600">{errors.description?.message}</p>
                                 <p className="mt-3 text-sm leading-6 text-gray-600">Write a few sentences about yourself.</p>
                             </div>
-                            <button className="inline-flex justify-center px-4 py-2.5 text-sm font-bold text-sky-100 bg-sky-500 border border-transparent rounded-md hover:bg-sky-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-sky-600 focus-visible:ring-offset-2" >Post blog</button>
+                            {/* Input Tag */}
+                            <div className="sm:col-span-full">
+                                <label htmlFor="tag_blog" className="block text-sm font-medium leading-6 text-gray-900">
+                                    Tag
+                                </label>
+                                <div className="mt-2">
+                                    <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-400 focus-within:ring-2 focus-within:ring-inset focus-within:ring-sky-500 sm:max-w-md">
+                                        <select className="block w-full text-gray-900 border-0 rounded-md shadow-sm ring-1 py-1.5 px-3.5 ring-inset ring-gray-400 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-500 sm:text-sm sm:leading-6" {...register('tag_blog')}>
+                                            <option value="">Choose the tag blog</option>
+                                            {tagsBlog.map((tag, index) => (
+                                                <option key={index} value={tag}>{tag}</option>
+                                            ))}
+                                        </select>
+                                    </div>
+                                    <p className="mt-2 text-sm text-red-600">{errors.tag_blog?.message}</p>
+                                </div>
+                            </div>
+                            {/* Input Photo */}
+                            <div className="col-span-full">
+                                <label htmlFor="photo_cover" className="block text-sm font-medium leading-6 text-gray-900">
+                                    Cover photo
+                                </label>
+                                <div className="flex justify-center px-6 py-10 mt-2 border border-dashed rounded-lg border-gray-900/25">
+                                    <div className="text-center">
+                                        {image ? (
+                                            <img src={image} alt="User's cover photo" className="object-cover w-full mx-auto rounded-lg h-96" />
+                                        ) : (
+                                            <>
+                                                <PhotoIcon className="w-12 h-12 mx-auto text-gray-300" aria-hidden="true" />
+                                                <div className="flex mt-4 text-sm leading-6 text-gray-600">
+                                                    <label
+                                                        htmlFor="photo_cover"
+                                                        className="relative font-semibold bg-white rounded-md cursor-pointer text-sky-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-sky-500 focus-within:ring-offset-2 hover:text-sky-800"
+                                                    >
+                                                        <span>Upload a file</span>
+                                                        <input
+                                                            id="photo_cover"
+                                                            name="photo_cover"
+                                                            type="file"
+                                                            {...register("photo_cover")}
+                                                            className="sr-only"
+                                                            onChange={handleFileChange}
+                                                        />
+                                                    </label>
+                                                    <p className="pl-1">or drag and drop</p>
+                                                </div>
+                                                <p className="text-xs leading-5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
+                                            </>
+                                        )}
+                                    </div>
+                                </div>
+                                <p className="mt-2 text-sm text-red-600">{errors.file_upload?.message}</p>
+                            </div>
+                            <button className="inline-flex justify-center px-4 py-2.5 text-sm font-bold text-sky-100 bg-sky-500 border border-transparent rounded-md hover:bg-sky-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-sky-600 focus-visible:ring-offset-2" onClick={openModal}>Post blog</button>
                         </div>
                     </div>
                 </div>
