f32a81580f24215f8b4a817c95ecc28f990a59d3
Andriawan Firmansyah <andriawnfirmansyah@gmail.com>
2023-11-20 12:16:45 +0700

    feat: add skeleton loading before display blogs

---
+++ src/components/SkeletonCard.jsx
@@ -0,0 +1,67 @@
+
+export default function SkeletonCard() {
+    return (
+        <section className="bg-gray-100">
+            <div className="container mx-auto animate-pulse">
+
+                <div className="grid max-w-2xl grid-cols-1 pt-10 mx-auto mt-10 border-t border-gray-300 gap-x-8 gap-y-16 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+                    <div className="w-full ">
+                        <div className="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-300"></div>
+
+                        <h1 className="w-56 h-2 mt-4 bg-gray-200 rounded-lg "></h1>
+                        <p className="w-24 h-2 mt-4 bg-gray-200 rounded-lg "></p>
+                    </div>
+
+                    <div className="w-full ">
+                        <div className="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-300"></div>
+
+                        <h1 className="w-56 h-2 mt-4 bg-gray-200 rounded-lg "></h1>
+                        <p className="w-24 h-2 mt-4 bg-gray-200 rounded-lg "></p>
+                    </div>
+
+                    <div className="w-full ">
+                        <div className="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-300"></div>
+
+                        <h1 className="w-56 h-2 mt-4 bg-gray-200 rounded-lg "></h1>
+                        <p className="w-24 h-2 mt-4 bg-gray-200 rounded-lg "></p>
+                    </div>
+
+                    <div className="w-full ">
+                        <div className="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-300"></div>
+
+                        <h1 className="w-56 h-2 mt-4 bg-gray-200 rounded-lg "></h1>
+                        <p className="w-24 h-2 mt-4 bg-gray-200 rounded-lg "></p>
+                    </div>
+
+                    <div className="w-full ">
+                        <div className="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-300"></div>
+
+                        <h1 className="w-56 h-2 mt-4 bg-gray-200 rounded-lg "></h1>
+                        <p className="w-24 h-2 mt-4 bg-gray-200 rounded-lg "></p>
+                    </div>
+
+                    <div className="w-full ">
+                        <div className="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-300"></div>
+
+                        <h1 className="w-56 h-2 mt-4 bg-gray-200 rounded-lg "></h1>
+                        <p className="w-24 h-2 mt-4 bg-gray-200 rounded-lg "></p>
+                    </div>
+
+                    <div className="w-full ">
+                        <div className="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-300"></div>
+
+                        <h1 className="w-56 h-2 mt-4 bg-gray-200 rounded-lg "></h1>
+                        <p className="w-24 h-2 mt-4 bg-gray-200 rounded-lg "></p>
+                    </div>
+
+                    <div className="w-full ">
+                        <div className="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-300"></div>
+
+                        <h1 className="w-56 h-2 mt-4 bg-gray-200 rounded-lg "></h1>
+                        <p className="w-24 h-2 mt-4 bg-gray-200 rounded-lg "></p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
--- src/pages/blogs/Blog.jsx
+++ src/pages/blogs/Blog.jsx
@@ -4,20 +4,30 @@ import CardBlog from "../../components/Card-Blog";
 import { getDocs, collection } from "firebase/firestore";
 import { db } from "../../config/firebase";
 import { useEffect } from "react";
+import SkeletonCard from "../../components/SkeletonCard";
 
 export default function Blog() {
-    
-    const [blogLists, setBlogLists] = useState(null);
-    
 
+    const [blogLists, setBlogLists] = useState(null);
+    const [loading, setLoading] = useState(true);
     const postRef = collection(db, "blogs-post");
 
-    const getBlogs = async () => {
-        const data = await getDocs(postRef);
-        setBlogLists(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    }
+
 
     useEffect(() => {
+        const getBlogs = async () => {
+            try {
+                const data = await getDocs(postRef);
+                setBlogLists(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+            } catch (error) {
+                console.error("Error fetching data details: ", error);
+            } finally {
+                setTimeout(() => {
+                    setLoading(false);
+                }, 2000);
+            }
+        };
+
         getBlogs();
     }, []);
 
@@ -28,11 +38,13 @@ export default function Blog() {
                     <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">From the blog</h2>
                     <p className="mt-2 text-lg leading-8 text-gray-600">Learn how to grow your business with our expert advice.</p>
                 </div>
-                <div className="grid max-w-2xl grid-cols-1 pt-10 mx-auto mt-10 border-t border-gray-300 gap-x-8 gap-y-16 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-                    {blogLists?.map((blog) =>
-                        <CardBlog key={blog.id} blog={blog} />
-                    )}
-                </div>
+                {loading ? (<SkeletonCard />) : (
+                    <div className="grid max-w-2xl grid-cols-1 pt-10 mx-auto mt-10 border-t border-gray-300 gap-x-8 gap-y-16 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+                        {blogLists?.map((blog) =>
+                            <CardBlog key={blog.id} blog={blog} />
+                        )}
+                    </div>
+                )}
             </div>
         </div>
     )
