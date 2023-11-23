71acf4567eb2a32f0ed82d3322b28f6542adc01a
Andriawan Firmansyah <andriawnfirmansyah@gmail.com>
2023-11-23 15:47:44 +0700

    feat: use context api for send all of datas

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
@@ -7,11 +8,34 @@ import PageNotFound from './pages/PageNotFound';
 import CreatePost from "./pages/create-blog/CreatePost";
 import BlogDetails from './pages/blogs/BlogDetails';
 import BlogList from './pages/blogs/BlogList';
+import { createContext, useState, useEffect } from "react";
+import { getDocs, collection } from "firebase/firestore";
+import { db } from './config/firebase';
+
+export const AppContext = createContext();
 
 function App() {
+  const [blogLists, setBlogLists] = useState(null);
+  const postRef = collection(db, "blogs-post");
+  const [IsLoading, setIsLoading] = useState(true);
 
+  useEffect(() => {
+    const getBlogs = async () => {
+      try {
+        const data = await getDocs(postRef);
+        setBlogLists(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      } catch (error) {
+        console.error("Error fetching data details: ", error);
+      } finally {
+        setTimeout(() => {
+          setIsLoading(false);
+        }, 1500);
+      }
+    };
+    getBlogs();
+  }, []);
   return (
-    <div className='container font-comfortaa'>
+    <AppContext.Provider value={{ blogLists, IsLoading, setBlogLists, setIsLoading }} className='container font-comfortaa'>
       <Router>
         <Navbar />
         <Routes>
@@ -23,7 +47,7 @@ function App() {
           <Route path='*' element={<PageNotFound />} />
         </Routes>
       </Router>
-    </div>
+    </AppContext.Provider>
   )
 }
 
